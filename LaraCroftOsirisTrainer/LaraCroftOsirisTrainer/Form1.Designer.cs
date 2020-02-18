9876afa51805090f349e27841afce72594af5907
Esto <MRCalderon3D@hotmail.es>
2020-02-18 12:43:27 +0100

    trainer full

---
+++ LaraCroftOsirisTrainer/LaraCroftOsirisTrainer/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace LaraCroftOsirisTrainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblCurrentAmmo = new System.Windows.Forms.Label();
+            this.chckAmmo = new System.Windows.Forms.CheckBox();
+            this.chkHealth = new System.Windows.Forms.CheckBox();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblGems = new System.Windows.Forms.Label();
+            this.txtGems = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // lblCurrentAmmo
+            // 
+            this.lblCurrentAmmo.AutoSize = true;
+            this.lblCurrentAmmo.Location = new System.Drawing.Point(88, 86);
+            this.lblCurrentAmmo.Name = "lblCurrentAmmo";
+            this.lblCurrentAmmo.Size = new System.Drawing.Size(35, 13);
+            this.lblCurrentAmmo.TabIndex = 4;
+            this.lblCurrentAmmo.Text = "label1";
+            // 
+            // chckAmmo
+            // 
+            this.chckAmmo.AutoSize = true;
+            this.chckAmmo.Location = new System.Drawing.Point(22, 86);
+            this.chckAmmo.Name = "chckAmmo";
+            this.chckAmmo.Size = new System.Drawing.Size(55, 17);
+            this.chckAmmo.TabIndex = 5;
+            this.chckAmmo.Text = "Ammo";
+            this.chckAmmo.UseVisualStyleBackColor = true;
+            // 
+            // chkHealth
+            // 
+            this.chkHealth.AutoSize = true;
+            this.chkHealth.Location = new System.Drawing.Point(22, 118);
+            this.chkHealth.Name = "chkHealth";
+            this.chkHealth.Size = new System.Drawing.Size(57, 17);
+            this.chkHealth.TabIndex = 7;
+            this.chkHealth.Text = "Health";
+            this.chkHealth.UseVisualStyleBackColor = true;
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.Location = new System.Drawing.Point(88, 118);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(35, 13);
+            this.lblHealth.TabIndex = 6;
+            this.lblHealth.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Gems";
+            // 
+            // lblGems
+            // 
+            this.lblGems.AutoSize = true;
+            this.lblGems.Location = new System.Drawing.Point(59, 159);
+            this.lblGems.Name = "lblGems";
+            this.lblGems.Size = new System.Drawing.Size(35, 13);
+            this.lblGems.TabIndex = 9;
+            this.lblGems.Text = "label1";
+            // 
+            // txtGems
+            // 
+            this.txtGems.Location = new System.Drawing.Point(100, 156);
+            this.txtGems.Name = "txtGems";
+            this.txtGems.Size = new System.Drawing.Size(43, 20);
+            this.txtGems.TabIndex = 10;
+            this.txtGems.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(151, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(42, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Set";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 220);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtGems);
+            this.Controls.Add(this.lblGems);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkHealth);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.chckAmmo);
+            this.Controls.Add(this.lblCurrentAmmo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblCurrentAmmo;
+        private System.Windows.Forms.CheckBox chckAmmo;
+        private System.Windows.Forms.CheckBox chkHealth;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblGems;
+        private System.Windows.Forms.TextBox txtGems;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LaraCroftOsirisTrainer/LaraCroftOsirisTrainer/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaraCroftOsirisTrainer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static bool botIsRunning;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            botIsRunning = true;
+            startNewThread();
+        }
+
+        public void startNewThread()
+        {
+            // Run the botmethods in a different thread, then the UI won't freeze.
+            // this starts the above method
+            Thread botMethod = new Thread(mainTrainerMethod);
+            botMethod.Start();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            botIsRunning = false;
+        }
+
+        IntPtr baseAddress = IntPtr.Zero;
+        public void mainTrainerMethod() // Focus world of warcraft windows
+        {
+            // var process = Process.GetProcesses();
+
+            var prc = Process.GetProcessesByName("LC2");
+            if (prc.Length > 0)
+            {
+                Process mainProcess = prc[0];
+                foreach (ProcessModule module in mainProcess.Modules)
+                {
+                    if (module.ModuleName.Contains("LC2"))
+                    {
+                        baseAddress = module.BaseAddress;
+                    }
+                }
+
+
+                VAMemory vMemory = new VAMemory("LC2");
+                int ammoFinalAddress = vMemory.ReadInt32((IntPtr)baseAddress + 0x056977B4); 
+                int healthFinalAddress = vMemory.ReadInt32((IntPtr)baseAddress + 0x025D1A00); 
+                int gemsFinalAddress = vMemory.ReadInt32((IntPtr)baseAddress + 0x00C0ACE8);
+
+                int ammoP1 = vMemory.ReadInt32((IntPtr)ammoFinalAddress + 0x14); // 808 // 288
+                int ammoP2 = vMemory.ReadInt32((IntPtr)ammoP1 + 0x0); // 808 // 288
+                int ammoOriginal = vMemory.ReadInt32((IntPtr)ammoP2 + 0x8); // 808 // 288
+                float healthOriginal = vMemory.ReadFloat((IntPtr)healthFinalAddress + 0x18); // 808 // 288
+                int gemsP1 = vMemory.ReadInt32((IntPtr)gemsFinalAddress + 0xc8); // 808 // 288
+                int gemsOriginal = vMemory.ReadInt32((IntPtr)gemsP1 + 0xe38); // 808 // 288
+
+
+                while (botIsRunning)
+                {
+                    // AMMO
+                    ammoOriginal = SetAmmo(vMemory, ammoP2, ammoOriginal);
+
+                    //HEALTH
+                    healthOriginal = SetHealth(vMemory, healthFinalAddress, healthOriginal);
+
+                    //GEMS
+                    int gems = vMemory.ReadInt32((IntPtr)gemsP1 + 0xe38);
+                    Invoke(new Action(() =>
+                    {
+                        lblGems.Text = gems.ToString();
+                    }));
+
+                    Thread.Sleep(200);
+                }
+                // SetForegroundWindow(prc[0].MainWindowHandle);
+            }
+            else
+                MessageBox.Show("LC2 window not found");
+
+        }
+
+        private float SetHealth(VAMemory vMemory, int healthFinalAddress, float healthOriginal)
+        {
+            float health = vMemory.ReadFloat((IntPtr)healthFinalAddress + 0x18); // 808 // 288
+            Invoke(new Action(() =>
+            {
+                lblHealth.Text = health.ToString();
+            }));
+            if (chkHealth.Checked)
+            {
+                if (health > healthOriginal)
+                {
+                    healthOriginal = health;
+                }
+
+                vMemory.WriteFloat((IntPtr)healthFinalAddress + 0x18, healthOriginal); // 808 // 288
+            }
+            else
+            {
+                healthOriginal = health;
+            }
+
+            return healthOriginal;
+        }
+
+        private int SetAmmo(VAMemory vMemory, int ammoP2, int ammoOriginal)
+        {
+            int ammo = vMemory.ReadInt32((IntPtr)ammoP2 + 0x8); // 808 // 288
+            Invoke(new Action(() =>
+            {
+                lblCurrentAmmo.Text = ammo.ToString();
+            }));
+            if (chckAmmo.Checked)
+            {
+                if (ammo > ammoOriginal)
+                {
+                    ammoOriginal = ammo;
+                }
+
+                vMemory.WriteInt32((IntPtr)ammoP2 + 0x8, ammoOriginal); // 808 // 288
+            }
+            else
+            {
+                ammoOriginal = ammo;
+            }
+
+            return ammoOriginal;
+        }
+        
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) )
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (baseAddress == IntPtr.Zero)
+            {
+                MessageBox.Show("Press Start");
+                return;
+            }
+               
+            VAMemory vMemory = new VAMemory("LC2");
+
+            int gemsFinalAddress = vMemory.ReadInt32((IntPtr)baseAddress + 0x00C0ACE8);
+
+            int gemsP1 = vMemory.ReadInt32((IntPtr)gemsFinalAddress + 0xc8); // 808 // 288
+            int gemsOriginal = vMemory.ReadInt32((IntPtr)gemsP1 + 0xe38); // 808 // 288
+
+            vMemory.WriteInt32((IntPtr)gemsP1 + 0xe38, int.Parse(txtGems.Text)); // 808 // 288
+
+
+            int gems = vMemory.ReadInt32((IntPtr)gemsP1 + 0xe38);
+            Invoke(new Action(() =>
+            {
+                lblCurrentAmmo.Text = gems.ToString();
+            }));
+
+        }
+    }
+}
